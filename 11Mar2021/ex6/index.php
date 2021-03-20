ad0aa5dd3ce6eb331372c5af63a4591579bbb0cf
nguyenthibich2102 <nguyenthibicht3h@gmail.com>
2021-03-20 14:02:01 +0700

    update EXE php

---
+++ 11Mar2021/ex6/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bai tap 6</title>
+</head>
+<body>
+	<?php
+	function group($arr, $elementNumber){
+		$countValue = count($arr);
+		$arrPush = array();
+		$arrResult = array();
+		for ($index=0; $index <$countValue ; $index++) { 
+			array_push($arrPush, $arr[$index]);
+			if (count(array_push($arrPush, $arr[$index])) == $elementNumber) {
+				array_push($arrResult, $arr[$index]);
+			}
+		}
+		foreach ($arrResult as $value) {
+			print_r($value);
+		}
+	}
+	$arr = [1,2,3,4,5,6,7,8,9];
+	$elementNumber = 2;
+	echo group($arr, $elementNumber);
+	?>
+</body>
+</html>
